bbb733dec20c66e5dd4f94e84402884fdcb4c430
Sachin Bhatia <sachin.bhatia@talentelgia.in>
2024-05-14 18:42:27 +0530

    Opportunities page HTML

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,63 @@
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image'; // Import Image from next/image
+import footerlogo from '../../public/images/giverr.svg';
+import insta from '../../public/images/instagram.svg';
+import twitter from '../../public/images/twitter.svg';
+import linkedin from '../../public/images/linkedin.svg';
+import link from '../../public/images/external-link.svg';
+
+const Footer: React.FC = () => {
+  return (
+    <div className="bg-[#E60054] text-center flex  items-center flex-col gap-10 pt-[100px] pb-12 px-10 relative before:content-[''] before:h-6 before:rounded-b-[24px] before:w-full before:absolute before:top-0 before:bg-[#F5F3EF]">
+      <div className="flex flex-col gap-2.5">
+        <h4 className="text-[#FFF3D680]">Company</h4>
+        <Link href="#" className="text-[#FFF3D6]  hover:underline">
+          About
+        </Link>
+        <Link href="#" className="text-[#FFF3D6] inline-flex  hover:underline">
+          Contact <Image src={link} alt="Link" />
+        </Link>
+      </div>
+
+      <div className="flex flex-col gap-2.5">
+        <h4 className="text-[#FFF3D680]">Legal</h4>
+        <Link href="#" className="text-[#FFF3D6] hover:underline">
+          Terms & Conditions
+        </Link>
+        <Link href="#" className="text-[#FFF3D6]  hover:underline">
+          Privacy Policy
+        </Link>
+      </div>
+
+      <div className="flex flex-col gap-2.5">
+        <h4 className="text-[#FFF3D680]">Social</h4>
+        <div className="flex gap-5 w-full justify-center">
+          <Link href="#">
+            <Image src={linkedin} alt="Linkedin" />
+          </Link>
+          <Link href="#">
+            <Image src={twitter} alt="Twitter" />
+          </Link>
+
+          <Link href="#">
+            <Image src={insta} alt="Instagram" />
+          </Link>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2.5 items-center mt-10">
+        <Link href="#" className="mb-4">
+          <Image src={footerlogo} alt="logo" />
+        </Link>
+
+        <p className="text-[#FFF3D6]">
+          © 2024 Giverr. <br></br>
+          All rights reserved.{' '}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/header/ProfileDropdown.tsx
@@ -0,0 +1,102 @@
+'use client';
+import { Fragment } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import Link from 'next/link';
+import check from '../../public/images/check-circle.svg';
+import Image from 'next/image'; // Import Image from next/image
+
+// import { ChevronDownIcon } from '@heroicons/react/20/solid';
+
+// function classNames(...classes: any) {
+//   return classes.filter(Boolean).join(' ');
+// }
+
+export default function ProfileDropdown() {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-full bg-[#BAA388] min-w-10 w-10 h-10 items-center text-base font-medium ">
+          A
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right !rounded-xl border !border-[#E6E3D6] !ring-0 bg-white !shadow-none">
+          <div className="p-1.5 flex flex-col gap-0.5">
+            <Menu.Item>
+              <Link
+                href="#"
+                className="flex items-center gap-2 text-base px-3	py-[7px] hover:bg-[#F5F3EF] rounded-lg"
+              >
+                <div className="w-5 h-5 rounded-full bg-[#BAA388] flex justify-center items-center text-xs">
+                  A
+                </div>{' '}
+                Anna Smith
+                <div className="ml-auto">
+                  <Image src={check} alt="check" />
+                </div>
+              </Link>
+            </Menu.Item>
+
+            <Menu.Item>
+              <Link
+                href="#"
+                className="flex items-center gap-2 text-base px-3	py-[7px] hover:bg-[#F5F3EF] rounded-lg mb-1"
+              >
+                <div className="w-5 h-5 rounded-full bg-[#88AEBA] flex justify-center items-center text-xs">
+                  H
+                </div>{' '}
+                Harmony
+              </Link>
+            </Menu.Item>
+            <hr className="border-[#E6E3D6] realtive -left-[1.5px] -right-[1.5px]"></hr>
+            <Menu.Item>
+              <Link
+                href="#"
+                className="flex items-center gap-2 text-base px-3	py-[7px] hover:bg-[#F5F3EF] rounded-lg"
+              >
+                Events & Activity
+              </Link>
+            </Menu.Item>
+
+            <Menu.Item>
+              <Link
+                href="#"
+                className="flex items-center gap-2 text-base px-3	py-[7px] hover:bg-[#F5F3EF] rounded-lg"
+              >
+                Wishlist
+              </Link>
+            </Menu.Item>
+            <hr className="border-[#E6E3D6]  relative -left-[1.5px] -right-[1.5px]"></hr>
+
+            <Menu.Item>
+              <Link
+                href="#"
+                className="flex items-center gap-2 text-base px-3	py-[7px] hover:bg-[#F5F3EF] rounded-lg"
+              >
+                Account settings
+              </Link>
+            </Menu.Item>
+
+            <Menu.Item>
+              <Link
+                href="#"
+                className="flex items-center gap-2 text-base px-3	py-[7px] hover:bg-[#F5F3EF] rounded-lg"
+              >
+                Log out
+              </Link>
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+}
