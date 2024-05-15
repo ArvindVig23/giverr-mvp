924449ddd1c7452227f6e29c7c713de6cd687a63
Sachin Bhatia <sachin.bhatia@talentelgia.in>
2024-05-15 18:05:20 +0530

    email functionality on sign up

---
+++ src/services/emailService.ts
@@ -0,0 +1,25 @@
+import { generateEmailTemplate } from '@/emailTemplate/signUpEmail';
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: +process.env.SMTP_PORT!,
+  auth: {
+    user: process.env.SMTP_EMAIL,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+export async function sendEmail(to: string, subject: string, text: string) {
+  try {
+    await transporter.sendMail({
+      from: process.env.SMTP_FROM, // sender address
+      to: to,
+      subject: subject,
+      text: text,
+      html: generateEmailTemplate(to),
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
