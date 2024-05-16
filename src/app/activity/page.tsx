c577f290d919705238c5b4b0049fa6be4cf5790f
Sachin Bhatia <sachin.bhatia@talentelgia.in>
2024-05-16 18:00:07 +0530

    Activity page & detail page html

---
+++ src/app/activity/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+import React from 'react';
+import ActivityTabs from '../../../components/allActivity/ActivityTabs';
+
+const Activity: React.FC = () => {
+  return <ActivityTabs />;
+};
+export default Activity;
