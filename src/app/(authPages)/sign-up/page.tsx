d82822c61b1c0fb0808a8d14fffa83ef42972f12
Sachin Bhatia <sachin.bhatia@talentelgia.in>
2024-05-16 16:18:00 +0530

    modified folder structure for hoc component

--- src/app/(authPages)/sign-up/page.tsx
+++ src/app/(authPages)/sign-up/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { useSearchParams } from 'next/navigation';
 import SignUpStep2 from '../../../../components/signUp/SignUpStep2';
 import CommonStep1 from '../../../../components/commonStep/CommonStep1';
-import { withAdminAuthorization } from '../../../../components/container/hocAuth';
+import { withAdminAuthorization } from '../../../../components/hoc/hocAuth';
 
 const SignUp: React.FC = () => {
   const searchParams = useSearchParams();
